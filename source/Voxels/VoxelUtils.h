bbb3f52d4d9e32322c3bfde5bca3580eca9d659f
CrazySiberianScientist <CrazySiberianScientist>
2024-11-18 00:35:02 +0700

    CSLineManipulator

--- source/Voxels/VoxelUtils.h
+++ source/Voxels/VoxelUtils.h
@@ -7,4 +7,5 @@ namespace VoxelProject
 {
 	// Только для воксельной сетки с только положительными координатами
 	float CalcRayDelta(const Vec3_meters& startPointLocal_meters, const Vec3_meters& rayDirLocal);
+	std::vector<Vec3_meters> RayTrace(const Vec3_meters& startPointLocal_meters, const Vec3_meters& rayDirLocal);
 }
