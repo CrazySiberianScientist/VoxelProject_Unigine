ad412b92a047823fb1a4216c15521e518019d2af
CrazySiberianScientist <CrazySiberianScientist>
2024-11-24 02:25:06 +0700

    поправил CalcRayDelta

--- source/Voxels/VoxelUtils.cpp
+++ source/Voxels/VoxelUtils.cpp
@@ -8,25 +8,20 @@ namespace VoxelProject
 	float CalcRayDelta(const Vec3_meters& startPointLocal_meters, const Vec3_meters& rayDirLocal)
 	{
 		auto calcLimit = [&rayDirLocal](const float v, const size_t index) {
-			//return (float)(int)(v + copysign(1.0f, rayDirLocal[index]));
-			const auto nextV = v + copysign(1.0f, rayDirLocal[index]);
-			return (rayDirLocal[index] >= 0.0) ? floor(nextV) : ceil(nextV);
+			return (rayDirLocal[index] >= 0.0) ? floor(v + 1.0f) : ceil(v - 1.0f);
 			};
 		const auto intersectLimits = MathUtils::MakeVecForEach<3>(startPointLocal_meters, calcLimit);
 
 		auto calcDistance = [&startPointLocal_meters, &rayDirLocal](const float limit, const size_t index) {
-			const auto distance = (limit - startPointLocal_meters[index]) / rayDirLocal[index];
-			// Когда оно слишком мало, то прибавление этого числа не влияет на другое число из-за чего получается бесконечный цикл
-			//return distance > FLT_EPSILON ? distance : FLT_MAX;
-			
-			assert(distance >= 0.0f);
-
-			return distance;
+			return (limit - startPointLocal_meters[index]) / rayDirLocal[index];
 			};
 		const auto distances = MathUtils::MakeVecForEach<3>(intersectLimits, calcDistance);
 
 		const float minDistance = std::min(distances[2], std::min(distances[0], distances[1]));
-		return std::max(minDistance, FLT_EPSILON * 100);
+
+		// При очень маленьком значении числа суммирование с этим числом не имеет эффекта, из-за чего получается бесконечный цикл
+		constexpr auto MAX_DISTANCE = FLT_EPSILON * 100;
+		return std::max(minDistance, MAX_DISTANCE);
 	}
 
 	std::vector<Vec3_meters> RayTrace(const Vec3_meters& startPointLocal, const Vec3_meters& rayDirLocal, const float maxDist)
