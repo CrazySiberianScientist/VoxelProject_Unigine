8bb05d5094133c5d9e4cb55a9642555e682fa3f3
CrazySiberianScientist <CrazySiberianScientist>
2025-03-02 01:58:43 +0700

    BitsetGetBit

--- data/Shaders/bitset.hlsl
+++ data/Shaders/bitset.hlsl
@@ -8,3 +8,11 @@ void SetBit(out uint value, const uint bitIndex, const bool bitValue)
     const uint mask = ~(1u << bitIndex);
     value = (value & mask) | (uint(bitValue) << bitIndex);
 }
+
+bool BitsetGetBit(StructuredBuffer<uint> bitset, const uint bitIndex)
+{
+    const uint blockSize_bits = 32;
+    const uint blockIndex = bitIndex / blockSize_bits;
+    const uint bitIndexInBlock = bitIndex % blockSize_bits;
+    return GetBit(blocks[blockIndex], bitIndex);
+}
--- data/Shaders/voxels_utils.hlsl
+++ data/Shaders/voxels_utils.hlsl
@@ -1,9 +1,9 @@
-int Pos3dToIndex(const float3 pos3d, const int xSize, const int ySize)
+uint Pos3dToIndex(const uint3 pos3d, const int xSize, const int ySize)
 {
     return pos3d[0] + pos3d[1] * xSize + pos3d[2] * xSize * ySize;
 }
 
-float3 IndexToPos3d(const int index, const int xSize, const int ySize)
+float3 IndexToPos3dFloat(const int index, const int xSize, const int ySize)
 {
     const int xySize = xSize * ySize;
     const float z = index / xySize;
@@ -12,4 +12,15 @@ float3 IndexToPos3d(const int index, const int xSize, const int ySize)
     const float y = indexXY / xSize;
 
     return float3(x, y, z);
+}
+
+int3 IndexToPos3dInt(const int index, const int xSize, const int ySize)
+{
+    const int xySize = xSize * ySize;
+    const int z = index / xySize;
+    const int indexXY = index % xySize;
+    const int x = indexXY % xSize;
+    const int y = indexXY / xSize;
+
+    return int3(x, y, z);
 }
