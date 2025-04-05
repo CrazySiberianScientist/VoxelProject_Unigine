f98852a28241b24bdc36e74f89e1c15a057b189a
CrazySiberianScientist <CrazySiberianScientist>
2025-04-05 16:10:20 +0700

    первая проба с кубом

--- data/Shaders/voxels_utils.h
+++ data/Shaders/voxels_utils.h
@@ -1,6 +1,12 @@
 #ifndef VOXEL_UTILS_H
 #define VOXEL_UTILS_H
 
+struct VoxelUtils__IntersectResult
+{
+	float3 points[2];
+	int isValid;
+};
+
 uint VoxelsUtils__Pos3dToIndex(const uint3 pos3d, const int xSize, const int ySize)
 {
     return pos3d[0] + pos3d[1] * xSize + pos3d[2] * xSize * ySize;
@@ -28,13 +34,6 @@ uint3 VoxelsUtils__IndexToPos3dInt(const uint index, const uint xSize, const uin
     return uint3(x, y, z);
 }
 
-/*
-uint3 VoxelRayTrace()
-{
-    
-}
-*/
-
 float2 VoxelsUtils__IntersectLineAABB(const float3 startPoint, const float3 dir, const float3 bbMin, const float3 bbMax)
 {
     // https://tavianator.com/2022/ray_box_boundary.html
@@ -59,20 +58,24 @@ bool VoxelsUtils__CheckIntersectLineTwoPoints(const float2 intersectLineResult)
 	return intersectLineResult[0] < intersectLineResult[1] && 0.0f <= intersectLineResult[1];
 }
 
-auto VoxelsUtils__IntersectSegmentAABB(const _Vec3& startPoint, const _Vec3& dir, const _Vec3& bbMin, const _Vec3& bbMax, const _ValueType &segmentLength)
+VoxelUtils__IntersectResult VoxelsUtils__IntersectSegmentAABB(const float3 startPoint, const float3 dir, const float3 bbMin, const float3 bbMax, const float segmentLength)
 {
-	const auto intersectLineResult = IntersectLineAABB<_Vec3, _ValueType>(startPoint, dir, bbMin, bbMax);
+	const float2 intersectLineResult = VoxelsUtils__IntersectLineAABB(startPoint, dir, bbMin, bbMax);
 
-	IntersectResult<_Vec3> result;
-	result.isValid = CheckIntersectLineTwoPoints(intersectLineResult) && (intersectLineResult[0] <= segmentLength);
+	VoxelUtils__IntersectResult result;
+	result.isValid = VoxelsUtils__CheckIntersectLineTwoPoints(intersectLineResult) && (intersectLineResult[0] <= segmentLength);
 
-	if (result.isValid)
-	{
-		result.points[0] = startPoint + dir * intersectLineResult[0] * (intersectLineResult[0] > _ValueType{});
-		result.points[1] = startPoint + dir * (intersectLineResult[1] > segmentLength ? segmentLength : intersectLineResult[1]);
-	}
+	result.points[0] = startPoint + dir * intersectLineResult[0] * (intersectLineResult[0] > 0.0f);
+	result.points[1] = startPoint + dir * (intersectLineResult[1] > segmentLength ? segmentLength : intersectLineResult[1]);
 
 	return result;
 }
+
+/*
+uint3 VoxelRayTrace()
+{
+    
+}
+*/
 
 #endif
